a0befdac5725e8ab7d516fbefffa30d07a69a1df
Edy Segura <edysegura@gmail.com>
2023-03-27 09:27:55 -0300

    feat: simulate an error in ContactController

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import FIRST_NAME_FIELD from "@salesforce/schema/Contact.FirstName";
 import LAST_NAME_FIELD from "@salesforce/schema/Contact.LastName";
 import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
 import getContacts from "@salesforce/apex/ContactController.getContacts";
+import { reduceErrors } from "c/ldsUtils";
 
 const COLUMNS = [
   {
@@ -26,4 +27,8 @@ export default class ContactList extends LightningElement {
   columns = COLUMNS;
   @wire(getContacts)
   contacts;
+
+  get errors() {
+    return this.contacts.error ? reduceErrors(this.contacts.error) : [];
+  }
 }
